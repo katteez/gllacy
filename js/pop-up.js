109bbc83f7cf09b64bfb3d5fde59b5f880d983d7
EkaterinaZoshchik <ekaterina.zoschik@gmail.com>
2016-11-15 00:20:02 +0300

    Google Maps API, оживление модального окна (js), анимация

---
+++ js/pop-up.js
@@ -0,0 +1,59 @@
+/********** Модальное окно "Обратная связь" **********/
+var btnOpenFeedback = document.querySelector("#button-open-feedback"),
+    popupFeedback = document.querySelector(".modal-feedback"),
+    btnCloseFeedback = document.querySelector(".modal-feedback .button-close"),
+    overlay = document.querySelector(".modal-overlay");
+    
+if (btnOpenFeedback && popupFeedback && btnCloseFeedback) {
+  form = popupFeedback.querySelector("form");
+  fieldName = popupFeedback.querySelector("#user-name");
+  fieldEmail = popupFeedback.querySelector("#feedback-email");
+  fieldText = popupFeedback.querySelector("#user-feedback");
+  
+  btnOpenFeedback.addEventListener("click", function (event) {
+    event.preventDefault();
+    popupFeedback.classList.add("show");
+    overlay.classList.add("show");
+    fieldName.focus();
+  });
+
+  form.addEventListener("submit", function (event) {
+    if (!fieldName.value || !fieldEmail.value || !fieldText.value) {
+      event.preventDefault();
+      popupFeedback.classList.remove("modal-error");
+      popupFeedback.offsetWidth = popupFeedback.offsetWidth;
+      popupFeedback.classList.add("modal-error");
+    }
+  });
+  btnCloseFeedback.addEventListener("click", function (event) {
+    event.preventDefault();
+    popupFeedback.classList.remove("show");
+    overlay.classList.remove("show");
+    popupFeedback.classList.remove("modal-error");
+  });
+  window.addEventListener("keydown", function (event) {
+    if (event.keyCode === 27) {
+      if (popupFeedback.classList.contains("show")) {
+        popupFeedback.classList.remove("show");
+        overlay.classList.remove("show");
+        popupFeedback.classList.remove("modal-error");
+      }
+    }
+  });
+}
+
+/********** Карта **********/
+window.onload = function () {
+  var mapOptions = {
+    zoom: 17,
+    center: new google.maps.LatLng(59.9389, 30.3259)
+  }
+  var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+  var image = "../img/map-marker.png";
+  var myLatLng = new google.maps.LatLng(59.9385, 30.3229);
+  var beachMarker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    icon: image
+  });
+}
